8eaea8ceb7b93fb1df80b85df1dc751621ed709d
Alex Wolf <contact@puppylab.net>
2026-01-04 20:10:50 -0500

    2025-1-1.10 Solution Found!
    Oh thank goodness, after forever, I found the answer. I am annoyed it
    took this long, but after switching to manually moving left/right, I was
    apparently counting the zero-crossings before the operation ended.

--- src/Y25/day1.rs
+++ src/Y25/day1.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 static INPUT_FILE: &str = include_str!("../../datafiles/Y25/day01-1.txt");
 
 struct NobOperation {
@@ -8,6 +10,15 @@ struct NobOperation {
 enum Direction {
     Clockwise,
     Counterclockwise
+}
+
+impl Display for Direction {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Clockwise => write!(f, "R"),
+            Self::Counterclockwise => write!(f, "L")
+        }
+    }
 }
 
 // A Struct to represent the dial and its rotation, and encapuslate its logic
@@ -29,31 +40,31 @@ impl DialRotation {
         }
     }
 
-    pub fn rotate_left(&mut self) {
+    pub fn rotate_counterclockwise(&mut self) {
         self.rotation = {
             if self.rotation == 0 { 99 } // If we're at 0, going one left means going to 99
             else { self.rotation - 1 } // If we're not at 0, we just subtract one to go left
         };
-        self.zero_check(); // Check if we're at zero
     }
 
-    pub fn rotate_right(&mut self) {
+    pub fn rotate_clockwise(&mut self) {
         self.rotation = {
             if self.rotation == 99 { 0 } // If we're at 99, going one right would reset us to 0
             else { self.rotation + 1 }  // If we're not at 99, we just add one to go right
         };
-        self.zero_check(); // Check if we're at zero
     }
 
     // A helper to rotate multiple times
     pub fn turn_nob(&mut self, ops: NobOperation) {
+        let iv = self.rotation;
         for _i in 0..ops.delta {
             match ops.direction {
-                Direction::Clockwise => self.rotate_right(),
-                Direction::Counterclockwise => self.rotate_left(),
+                Direction::Clockwise => self.rotate_clockwise(),
+                Direction::Counterclockwise => self.rotate_counterclockwise(),
             }
-            println!("[Dial] {}", self.rotation)
         }
+        self.zero_check(); // Check if we're at zero
+        println!("Given a delta of ({},{}), rotated from ({}) to ({})", ops.direction, ops.delta, iv, self.rotation,);
     }
 }
 
@@ -62,11 +73,12 @@ fn parse_line(line: &str) -> NobOperation {
     let direction_val_str = line.get(1..).unwrap();
     let direction_val: usize = direction_val_str.parse().expect("Unable to parse rotation");
 
-    println!("({}){}", direction_sym, direction_val);
+    // println!("({}){}", direction_sym, direction_val);
     
     let direction = match direction_sym {
         "L" => Direction::Counterclockwise,
-        _ => Direction::Clockwise
+        "R" => Direction::Clockwise,
+        _ => panic!("Unknown value")
     };
 
     NobOperation { delta: direction_val, direction }
